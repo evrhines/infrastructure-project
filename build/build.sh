eb7c4db5a6cdb1b95062e8f79e6633b122394ac3
Evan Hines <evrhiness@gmail.com>
2021-12-31 20:00:09 -0500

    Remove cache filter because those will never be pushed and . is being considered a regex

--- build/build.sh
+++ build/build.sh
@@ -1,14 +1,14 @@
 
 for CHANGED in "$CHANGED_FILES"; do
     echo "$CHANGED"
-    if [ $(echo "$CHANGED" | grep 'terragrunt.hcl' | grep -v '.terragrunt' | grep -v 'infra/terragrunt.hcl') ]; then
+    if [ $(echo "$CHANGED" | grep 'terragrunt.hcl' | grep -v 'infra/terragrunt.hcl') ]; then
         terragrunt apply --terragrunt-working-dir "${CHANGED%\/*}" --terragrunt-non-interactive -auto-approve
     fi
 done
 
 for DELETED in "$DELETED_FILES"; do
     echo "$DELETED"
-    if [ $(echo "$DELETED" | grep 'terragrunt.hcl' | grep -v '.terragrunt' | grep -v 'infra/terragrunt.hcl') ]; then
+    if [ $(echo "$DELETED" | grep 'terragrunt.hcl' | grep -v 'infra/terragrunt.hcl') ]; then
         terragrunt apply --terragrunt-working-dir "${DELETED%\/*}" --terragrunt-non-interactive -auto-approve
     fi
 done

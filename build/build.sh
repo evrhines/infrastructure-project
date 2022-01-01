081d53ec10d165592266afffbff521730a749bf5
Evan Hines <evrhiness@gmail.com>
2021-12-31 19:50:38 -0500

    Attempt to fix up CD

--- build/build.sh
+++ build/build.sh
@@ -1,12 +1,9 @@
-CHANGED_FILES=$(cat "$HOME"/files_added.json "$HOME"/files_modified.json "$HOME"/files_renamed.json  | jq '.[]' | grep terragrunt.hcl | grep -v ".terragrunt-cache" | grep -v "infra/terragrunt.hcl" | xargs)
-DELETED_FILES=$(cat "$HOME"/files_deleted.json | jq '.[]' | grep terragrunt.hcl | grep -v ".terragrunt-cache" | grep -v "infra/terragrunt.hcl" | xargs)
-
 for CHANGED in "$CHANGED_FILES"; do
     echo "$CHANGED"
-    #terragrunt apply --terragrunt-working-dir "${CHANGED%\/*}" --terragrunt-non-interactive -auto-approve
+    terragrunt apply --terragrunt-working-dir "${CHANGED%\/*}" --terragrunt-non-interactive -auto-approve
 done
 
 for DELETED in "$DELETED_FILES"; do
     echo "$DELETED"
-    #terragrunt destroy --terragrunt-working-dir "${DELETED%\/*}" --terragrunt-non-interactive -auto-approve
+    terragrunt destroy --terragrunt-working-dir "${DELETED%\/*}" --terragrunt-non-interactive -auto-approve
 done
